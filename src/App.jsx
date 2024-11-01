929b092579a0951331f905d66f5405528b7640b3
Batuhan Yildirim <bthn.yildirim@gmail.com>
2024-11-01 13:07:14 +0100

    connected API and listed items

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.arbeitnow.com/api/job-board-api")
+      .then((response) => {
+        setJobs(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <>
-      <h1>Path Foundry</h1>
-    </>
+    <div>
+      <h1>Job Listings</h1>
+      <ul>
+        {jobs.map((job) => (
+          <li key={job.id}>
+            <h2>{job.title}</h2>
+            <p>
+              <strong>Company:</strong> {job.company_name}
+            </p>
+            <p>
+              <strong>Location:</strong> {job.location}
+            </p>
+            <a href={job.url} target="_blank" rel="">
+              View Job
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
