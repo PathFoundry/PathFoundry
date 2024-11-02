c2fa44c75e5c7df615d7642339dcc5a0f81f8038
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2024-11-02 09:06:09 +0100

    add basic job card css

--- src/components/JobsList.jsx
+++ src/components/JobsList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "../css/JobsList.css";
 
 function JobsList() {
   const [jobs, setJobs] = useState([]);
@@ -23,16 +24,34 @@ function JobsList() {
   return (
     <div>
       <h1>Job Listings</h1>
-      <ul>
+      <ul className="jobs-list-container">
         {jobs.map((job) => (
           <li key={job.id}>
-            <h2>{job.salary}</h2>
-            <p>
-              <strong>Company:</strong> {job.description}
-            </p>
-            <p>
-              <strong>Location:</strong> {job.location}
-            </p>
+            <div className="job-card-header">
+              <div className="job-card-logo">
+                <img src={job.company_logo_url} />
+              </div>
+              <div className="job-card-basic-info">
+                <div className="job-card-title">
+                  <h2 id="job-name">{job.job_name}</h2>
+                  <p id="company-name"> {job.company_name} </p>
+                  <p id="company-location"> {job.company_location} </p>
+                </div>
+                <div className="job-card-salary">
+                  <p id="job-salary">{job.salary} </p>
+                </div>
+              </div>
+            </div>
+            <div className="job-card-bottom">
+              <div className="job-card-description">
+                <p id="job-description">{job.description} </p>
+              </div>
+              <div className="job-card-structured-data">
+                <p id="contract-type">{job.type_contract} </p>
+                {job.remote === true && <p>Remote</p>}
+                <p id="job-hours">{job.job_hours} </p>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
