b8849be2a6480453f2113a4a6d2ec530ad2121b4
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2024-11-04 22:44:09 +0100

    style about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,37 +1,69 @@
 function About() {
   return (
     <div className="list-container">
-      <h1>About Us</h1>
-      <div>
-        <h4>What we do</h4>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lorem
-          lectus. Aenean ac massa dictum, pellentesque est eget, fringilla arcu.
-          Nullam a ipsum quis est dictum venenatis eget vitae justo. Suspendisse
-          a faucibus libero.
-        </p>
-      </div>
-      <div>
-        <h4>Our Goals</h4>
-        <p>
-          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-          posuere cubilia curae; Praesent tincidunt nisi quis leo iaculis
-          vulputate. Aliquam est purus, tempus vitae commodo in, consequat in
-          urna.
-        </p>
-      </div>
-      <div>
-        <h4>Who we are</h4>
-        <h6>Contributor 1</h6>
-        <p>
-          Praesent et vehicula lacus. Praesent eget aliquet nisi. Aenean laoreet
-          faucibus enim in vehicula. In a interdum velit, ac suscipit mauris.
-        </p>
-        <h6>Contributor 2</h6>
-        <p>
-          Praesent et vehicula lacus. Praesent eget aliquet nisi. Aenean laoreet
-          faucibus enim in vehicula. In a interdum velit, ac suscipit mauris.
-        </p>
+      <div className="flex-container">
+        <h1>About Us</h1>
+        <div className="what-container">
+          <h4>What we do</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec
+            lorem lectus. Aenean ac massa dictum, pellentesque est eget,
+            fringilla arcu. Nullam a ipsum quis est dictum venenatis eget vitae
+            justo. Suspendisse a faucibus libero.
+          </p>
+        </div>
+        <div className="goals-container">
+          <h4>Our Goals</h4>
+          <p>
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+            posuere cubilia curae; Praesent tincidunt nisi quis leo iaculis
+            vulputate. Aliquam est purus, tempus vitae commodo in, consequat in
+            urna.
+          </p>
+        </div>
+        <div className="who-container">
+          <h4>Who we are</h4>
+          <div className="contributors-container">
+            <div className="contributor-1">
+              <h6>Batu</h6>
+              <img className="photo" src="src/assets/sample-photo1.jpg" />
+              <p>
+                Praesent et vehicula lacus. Praesent eget aliquet nisi. Aenean
+                laoreet faucibus enim in vehicula. In a interdum velit, ac
+                suscipit mauris.
+              </p>
+              <div className="logos">
+                <img
+                  className="github-logo"
+                  src="src/assets/github-logo.webp"
+                />
+                <img
+                  className="linkedin-logo"
+                  src="src/assets/linkedin-logo.webp"
+                />
+              </div>
+            </div>
+            <div className="contributor-2">
+              <h6>Olga</h6>
+              <img className="photo" src="src/assets/sample-photo2.jpg" />
+              <p>
+                Praesent et vehicula lacus. Praesent eget aliquet nisi. Aenean
+                laoreet faucibus enim in vehicula. In a interdum velit, ac
+                suscipit mauris.
+              </p>
+              <div className="logos">
+                <img
+                  className="github-logo"
+                  src="src/assets/github-logo.webp"
+                />
+                <img
+                  className="linkedin-logo"
+                  src="src/assets/linkedin-logo.webp"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/JobsList.jsx
+++ src/components/JobsList.jsx
@@ -26,47 +26,51 @@ function JobsList(props) {
   }, []);
 
   return (
-    <div className="px-4 py-8" className="list-container">
-      <h1 className="text-2xl font-bold mb-6">Job Listings</h1>
-      <ul className="space-y-4">
-        {(props.jobs || []).map((job) => (
-          <li
-            key={job.id}
-            className="bg-white shadow-lg rounded-lg p-6 flex flex-col md:flex-row"
-          >
-            <div className="flex-shrink-0">
-              <img
-                src={job.company_logo_url}
-                alt={`${job.company_name} logo`}
-                className="w-20 h-20 rounded-md border border-blue-500"
-              />
-            </div>
-            <div className="flex-grow md:ml-4 mt-4 md:mt-0">
-              <h2 className="text-xl font-semibold text-gray-800">
-                {job.job_name}
-              </h2>
-              <p className="text-gray-600">{job.company_name}</p>
-              <p className="text-gray-500">{job.company_location}</p>
-              <p className="mt-2 text-lg font-bold text-gray-700">
-                {job.salary}
-              </p>
-              <div className="mt-4 flex items-center justify-between">
-                <div className="flex space-x-4 text-gray-500">
-                  <span>{job.type_contract}</span>
-                  {job.remote && <span className="text-green-500">Remote</span>}
-                  <span>{job.job_hours}</span>
-                </div>
-                <Link
-                  to={`/jobs/${job.id}`}
-                  className="mt-2 text-blue-500 hover:text-blue-700 font-semibold"
-                >
-                  View More Details
-                </Link>
+    <div className="list-container">
+      <div className="px-4 py-8">
+        <h1 className="text-2xl font-bold mb-6">Job Listings</h1>
+        <ul className="space-y-4">
+          {(props.jobs || []).map((job) => (
+            <li
+              key={job.id}
+              className="bg-white shadow-lg rounded-lg p-6 flex flex-col md:flex-row"
+            >
+              <div className="flex-shrink-0">
+                <img
+                  src={job.company_logo_url}
+                  alt={`${job.company_name} logo`}
+                  className="w-20 h-20 rounded-md border border-blue-500"
+                />
               </div>
-            </div>
-          </li>
-        ))}
-      </ul>
+              <div className="flex-grow md:ml-4 mt-4 md:mt-0">
+                <h2 className="text-xl font-semibold text-gray-800">
+                  {job.job_name}
+                </h2>
+                <p className="text-gray-600">{job.company_name}</p>
+                <p className="text-gray-500">{job.company_location}</p>
+                <p className="mt-2 text-lg font-bold text-gray-700">
+                  {job.salary}
+                </p>
+                <div className="mt-4 flex items-center justify-between">
+                  <div className="flex space-x-4 text-gray-500">
+                    <span>{job.type_contract}</span>
+                    {job.remote && (
+                      <span className="text-green-500">Remote</span>
+                    )}
+                    <span>{job.job_hours}</span>
+                  </div>
+                  <Link
+                    to={`/jobs/${job.id}`}
+                    className="mt-2 text-blue-500 hover:text-blue-700 font-semibold"
+                  >
+                    View More Details
+                  </Link>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
