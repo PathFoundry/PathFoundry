e4c75e746464a6a2fce28f9995bfbdf595f65c68
Olga Casanovas Monclus <o.casanovas.m@gmail.com>
2024-11-06 13:04:03 +0100

    add changes to about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -26,40 +26,54 @@ function About() {
           <div className="contributors-container">
             <div className="contributor-1">
               <h6>Batu</h6>
-              <img className="photo" src="src/assets/sample-photo1.jpg" />
+              <img className="photo" src="src/assets/photo-batu.png" />
               <p>
                 Praesent et vehicula lacus. Praesent eget aliquet nisi. Aenean
                 laoreet faucibus enim in vehicula. In a interdum velit, ac
                 suscipit mauris.
               </p>
               <div className="logos">
-                <img
-                  className="github-logo"
-                  src="src/assets/github-logo.webp"
-                />
-                <img
-                  className="linkedin-logo"
-                  src="src/assets/linkedin-logo.webp"
-                />
+                <a href="https://github.com/bthnyildirim" target="blank">
+                  <img
+                    className="github-logo"
+                    src="src/assets/github-logo.webp"
+                  />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/batuhan-yildirim-17b109124/"
+                  target="blank"
+                >
+                  <img
+                    className="linkedin-logo"
+                    src="src/assets/linkedin-logo.webp"
+                  />
+                </a>
               </div>
             </div>
             <div className="contributor-2">
               <h6>Olga</h6>
-              <img className="photo" src="src/assets/sample-photo2.jpg" />
+              <img className="photo" src="src/assets/stuart-photo.png" />
               <p>
                 Praesent et vehicula lacus. Praesent eget aliquet nisi. Aenean
                 laoreet faucibus enim in vehicula. In a interdum velit, ac
                 suscipit mauris.
               </p>
               <div className="logos">
-                <img
-                  className="github-logo"
-                  src="src/assets/github-logo.webp"
-                />
-                <img
-                  className="linkedin-logo"
-                  src="src/assets/linkedin-logo.webp"
-                />
+                <a href="https://github.com/OlgaCasanovasM" target="blank">
+                  <img
+                    className="github-logo"
+                    src="src/assets/github-logo.webp"
+                  />
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/olga-casanovas-bb29bb89/"
+                  target="blank"
+                >
+                  <img
+                    className="linkedin-logo"
+                    src="src/assets/linkedin-logo.webp"
+                  />
+                </a>
               </div>
             </div>
           </div>
